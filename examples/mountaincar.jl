a5d7ac52f0986eeb36d7f361c398510f112507ae
Johanni Brea <jbrea@users.noreply.github.com>
2018-08-10 14:13:50 +0200

    mountain car with sarsa(lambda) and tile coding

--- examples/mountaincar.jl
+++ examples/mountaincar.jl
@@ -1,15 +1,21 @@
 using RLEnvClassicControl
 
-env = MountainCar()
-rlsetup = RLSetup(ActorCriticPolicyGradient(ns = 2, na = 3, α = .02, 
-                                            nsteps = 25), 
-                  env, ConstantNumberSteps(400), 
+env = MountainCar(maxsteps = 10^4)
+high = [.5, .07]
+low = [-1.2, -.07]
+nbins = [8, 8]
+p0 = StateAggregator(low, high, nbins)
+preprocessor = TilingStateAggregator(p0, 8)
+rlsetup = RLSetup(Sarsa(ns = 8*8^2, na = 3, α = 1/8, λ = .96, γ = 1.), 
+                  env, ConstantNumberSteps(400),
+                  preprocessor = preprocessor,
+                  policy = EpsilonGreedyPolicy(0),
                   callbacks = [Visualize(wait = .02)])
 info("Before learning.") 
 run!(rlsetup)
-rlsetup.callbacks = [EvaluationPerEpisode(TotalReward())]
+rlsetup.callbacks = [EvaluationPerEpisode(TimeSteps())]
 rlsetup.stoppingcriterion = ConstantNumberSteps(10^5)
-learn!(rlsetup)
+@time learn!(rlsetup)
 getvalue(rlsetup.callbacks[1])
 info("After learning.")
 rlsetup.callbacks = [Visualize(wait = .02)]
